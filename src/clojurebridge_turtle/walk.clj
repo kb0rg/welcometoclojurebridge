704eef815ef40a44a85e5008f288901d40cdfd13
Yoko Harada <yokolet@gmail.com>
2016-09-02 16:45:23 -0400

    Updates comment to meet with NC 2.1.0

--- src/clojurebridge_turtle/walk.clj
+++ src/clojurebridge_turtle/walk.clj
@@ -4,15 +4,16 @@
 (init)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; On NightCode, click Run with REPL followed by Reload for the
-;; first time. If REPL has already started, click Reload to run
-;; this code.
+;; On NightCode, click Run with REPL followed by Reload File
+;; for the first time. If REPL has already started, click
+;; Reload File to run this code.
 ;;
 ;; Once a turtle (small triangle) appears in the center of window,
 ;; type code in REPL.
 ;;
-;; Alternatively, you may type code in this editor. To evaluate
-;; whole file, click Reload.
+;; Alternatively, you may type code in this editor. To evaluate:
+;; - pieces of code: select code fragment and Reload Selection
+;; - whole file: click Reload File
 ;;
 ;; [Exercise]
 ;; write some functions under these comment lines
